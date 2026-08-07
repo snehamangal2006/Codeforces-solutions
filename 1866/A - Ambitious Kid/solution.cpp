e1a863cee6392bc297a60891b23ef34663301285
snehamangal2006 <snehamangal9446@gmail.com>
2026-08-07 12:30:52 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+int n;
+cin>>n;
+int arr[n];
+int mini=INT_MAX;
+ 
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+    mini=min(mini,abs(arr[i]-0));
+} cout<<mini;
+ 
+    return 0;
+}
