bc6fa59c45a0a4a4e3e70a16a2b1f6a5be20e42e
snehamangal2006 <snehamangal9446@gmail.com>
2026-08-12 00:01:03 +0530

    Add One and Two [A] from Codeforces

---
+++ 1788/A - One and Two/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        int total2 = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+ 
+            if (a[i] == 2)
+                total2++;
+        }
+ 
+        // Only 1s
+        if (total2 == 0) {
+            cout << 1 << endl;
+            continue;
+        }
+ 
+        // Odd number of 2s
+        if (total2 % 2 != 0) {
+            cout << -1 << endl;
+            continue;
+        }
+ 
+        int need = total2 / 2;
+        int count2 = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 2) {
+                count2++;
+ 
+                if (count2 == need) {
+                    cout << i + 1 << endl;
+                    break;
+                }
+            }
+        }
+    }
+ 
+    return 0;
+}
