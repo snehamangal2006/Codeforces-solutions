cd2cd25ab57d6f01590d98eaa30ec455c3e9a142
snehamangal2006 <snehamangal9446@gmail.com>
2026-08-19 01:16:37 +0530

    Add Halloumi Boxes [A] from Codeforces

--- 1903/A - Halloumi Boxes/solution.cpp
+++ 1903/A - Halloumi Boxes/solution.cpp
@@ -2,33 +2,26 @@
 using namespace std;
  
 int main() {
-    int t;
-    cin >> t;
+int t;
+cin>>t;
+while(t--){
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } if(k>1) cout<<"YES"<<endl;
+    else {
+        bool found=true;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                
+            found = false;
+            break;
+       } } if(found) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+        } 
+    } return 0;
  
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
  
-        vector<int> arr(n), b;
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
- 
-        if (k > 1) {
-            cout << "YES
-";
-        } else {
-            b = arr;
-            sort(b.begin(), b.end());
- 
-            if (arr == b)
-                cout << "YES
-";
-            else
-                cout << "NO
-";
-        }
-    }
- 
-    return 0;
 }
